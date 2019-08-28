b811cbf05f8ff533fb19d049cd608066dab23a5e
Koliesnik Yevhenii <dp260793kev@yandex.ua>
2019-08-28 19:18:20 +0300

    ttt

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleRelease2
             Console.WriteLine("Vasyl Travin testing changes.");
 
             ArraInt array = new ArraInt(10);
-            Console.WriteLine("d");
+            Console.WriteLine("dff");
 
             Console.WriteLine(array.GetSum());
 
